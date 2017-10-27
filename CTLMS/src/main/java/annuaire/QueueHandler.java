21e75410e3ea9d172555e4b258aa56bbf1756528
Alexandre.bar <alexandre.bar83@gmail.com>
2017-10-27 15:49:53 +0200

    Refactoring

--- CTLMS/src/main/java/annuaire/QueueHandler.java
+++ CTLMS/src/main/java/annuaire/QueueHandler.java
@@ -1,48 +1,65 @@
+package annuaire;
 
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import gestionpattern.Pattern;
 
-import java.io.UnsupportedEncodingException;
-
+/**
+ * Classe d'abstraction faisant l'interface avec RabbitMQ
+ */
 public class QueueHandler {
 
     private static final String EXCHANGE_NAME = "ctlms_exchanger";
 
     private Channel channel;
 
+    private static String DEFAULTFLAG = "ALL";
+
     public static void main(String[] argv) throws Exception {
         QueueHandler qh = new QueueHandler();
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
+        //On se connecte a rabbitMQ et on y crée un channel.
         qh.channel = connection.createChannel();
-
+        //On déclare l'échangeur de type que l'on va utiliser pour nos messages(ctlms_exchanger).
         qh.channel.exchangeDeclare(EXCHANGE_NAME, "direct");
 
-        qh.sendMessage(argv);
 
+        //Ici à terme on aura théoriquement une boucle qui attends qu'on lui dise d'envoyer des messages.
+        //et qui les enverra comme ci dessous:
+        qh.sendMessage(getFlags(argv), getMessage(argv));
+
+
+        //Fermeture du channel et de la connexion.
         qh.channel.close();
         connection.close();
     }
 
-    public void sendMessage(String[] strings) throws Exception {
-        String flags = getFlags(strings);      //flags of queues to send message
-        String message = getMessage(strings);  //message (JSON)
+    /**
+     * The method used to send messages to rabbitMQ
+     * @param flag the flag where messages will be sent (Used by rabbitMQ to route Messages)
+     * @param message the message containing JSON
+     * @throws Exception
+     */
 
-        this.channel.basicPublish(EXCHANGE_NAME, flags, null, message.getBytes("UTF-8"));
-        System.out.println(" [x] Sent '" + flags + "':'" + message + "'");
+    public void sendMessage(String flag , String message) throws Exception {
+        //Envoie un message à un certain exchanger, avec un certain flag.
+        this.channel.basicPublish(EXCHANGE_NAME, flag, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + flag + "':'" + message + "'");
     }
 
     private static String getFlags(String[] strings){
         if (strings.length < 1)
-            return "info";
+            return DEFAULTFLAG;
         return strings[0];
     }
 
     private static String getMessage(String[] strings){
+        Pattern pattern = new Pattern();
         if (strings.length < 2)
-            return "Hello World!";
+            return pattern.getDescription();
         return joinStrings(strings, " ", 1);
     }
 
@@ -63,6 +80,7 @@ public class QueueHandler {
     }
 
     public QueueHandler() {
+
     }
 
 }
