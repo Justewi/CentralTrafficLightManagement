abd7d1e429cec12ff4d8bc6a3b8a6f1accb6b19b
Alexandre.bar <alexandre.bar83@gmail.com>
2017-10-27 11:35:20 +0200

    Ajout du queue handler pour la partie java

--- CTLMS/src/main/java/annuaire/QueueHandler.java
+++ CTLMS/src/main/java/annuaire/QueueHandler.java
@@ -3,30 +3,38 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
-public class EmitLogDirect {
+import java.io.UnsupportedEncodingException;
+
+public class QueueHandler {
 
     private static final String EXCHANGE_NAME = "ctlms_exchanger";
 
-    public static void main(String[] argv) throws Exception {
+    private Channel channel;
 
+    public static void main(String[] argv) throws Exception {
+        QueueHandler qh = new QueueHandler();
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        qh.channel = connection.createChannel();
 
-        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        qh.channel.exchangeDeclare(EXCHANGE_NAME, "direct");
 
-        String severity = getSeverity(argv);
-        String message = getMessage(argv);
+        qh.sendMessage(argv);
 
-        channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8"));
-        System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
-
-        channel.close();
+        qh.channel.close();
         connection.close();
     }
 
-    private static String getSeverity(String[] strings){
+    public void sendMessage(String[] strings) throws Exception {
+        String flags = getFlags(strings);      //flags of queues to send message
+        String message = getMessage(strings);  //message (JSON)
+
+        this.channel.basicPublish(EXCHANGE_NAME, flags, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + flags + "':'" + message + "'");
+    }
+
+    private static String getFlags(String[] strings){
         if (strings.length < 1)
             return "info";
         return strings[0];
@@ -42,12 +50,19 @@ public class EmitLogDirect {
         int length = strings.length;
         if (length == 0 ) return "";
         if (length < startIndex ) return "";
-
-        //
         StringBuilder words = new StringBuilder(strings[startIndex]);
         for (int i = startIndex + 1; i < length; i++) {
             words.append(delimiter).append(strings[i]);
         }
         return words.toString();
     }
+
+
+    public QueueHandler(Channel channel) {
+        this.channel = channel;
+    }
+
+    public QueueHandler() {
+    }
+
 }
