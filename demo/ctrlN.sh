71d28ce3081c9c7b7fe3610871089b35e2e69d07
Adrien Dengreville <didienv2@gmail.com>
2017-11-02 22:34:18 +0100

    Fixed docker network name issue

--- demo/ctrlN.sh
+++ demo/ctrlN.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run --network=al_default \
+docker run --network=street_network \
   -it --rm \
   -e "ID=controller_${1}" -e "CS=activemq" -e "CS_PORT=5672" \
   --name=controller_${1} \
