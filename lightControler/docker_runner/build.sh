77adc57ef4c85e8f7466858af7ccdeeb4bdc7cd3
Adrien Dengreville <didienv2@gmail.com>
2017-11-02 17:58:26 +0100

    Dockerized the controller stuff

--- lightControler/docker_runner/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ../docker_builder/dependencies.sh cpy_dependencies.sh
-docker build -t gr2/cppbuilder .
-rm cpy_dependencies.sh
--- lightControler/full_build.sh
+++ lightControler/full_build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cd docker_builder
+./build.sh
+cd ..
+
 docker build -t gr7/controller
