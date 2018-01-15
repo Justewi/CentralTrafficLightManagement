d3ce6d6703513cbab845b80197265f9c76e3f766
Alex Bar <alexandre.bar83@gmail.com>
2018-01-15 11:51:17 +0100

    Préparation de la mise en place du MVP

--- CTLMS/src/main/java/Application.java
+++ CTLMS/src/main/java/Application.java
@@ -12,6 +12,9 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+    private static final String DEFAULT_RABBITMQ_IP = "localhost";
+    private static final int DEFAULT_RABBITMQ_PORT = 5672;
+
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
@@ -50,6 +53,47 @@ public class Application {
                 log.info(bauer.toString());
             }
             log.info("");
+
+
+
+
+            //A adapter avec la couche de persistance et pas controler list
+
+            String host = DEFAULT_RABBITMQ_IP;
+            if (argv.length > 0) {
+                host = argv[0];
+            }
+            System.out.println("Connexion a RabbitMQ a l'adresse " + host + ":" + DEFAULT_RABBITMQ_PORT + " ...");
+
+            final QueueHandler qh = new QueueHandler(host);
+
+            System.out.println("Serveur connecte");
+
+            //Ici à terme on aura théoriquement une boucle qui attends qu'on lui dise d'envoyer des messages.
+            //et qui les enverra comme ci dessous:
+            System.out.println("Envoi des patterns aux controleurs connus");
+            ControlerList.sendInitPattern(qh);
+            System.out.println("Patterns initiaux envoyes");
+
+            System.out.println(" [*] Attente de message. CTRL+C pour quitter");
+
+            qh.onMessage(new MessageListener() {
+                @Override
+                public void onMessage(String tag, byte[] body) {
+                    try {
+                        String message = new String(body, "UTF-8");
+                        System.out.println(" [x] Received '" + message + "'");
+                        ControlerList.sendModifiedPattern(qh);
+                        System.out.println("");
+                    } catch (UnsupportedEncodingException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+
+            // TODO: Things ? Or a better way to do this.
+            while (true);
+
         };
     }
 
