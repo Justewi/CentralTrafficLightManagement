7f0efb26a921d96aabc2d2b8f4cc717bec372b7e
Adrien Dengreville <didienv2@gmail.com>
2018-02-09 10:54:20 +0100

    Updated benchmarks

--- lightControler/benchify.sh
+++ lightControler/benchify.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-size=(10 120 160 1 20 40 5 80 250)
+size=(1 5 10 20 40 80 100 120 160 250 500 750 1000)
 
 #https://stackoverflow.com/questions/1527049/join-elements-of-an-array
 function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
 
 for i in "${size[@]}"
 do :
-  ./startControlers.sh $i localhost 5672 > bench/bench_$i.log
+  echo "Start test for $i"
+  ./startControlers.sh $i $1 5672 > bench/bench_$i.log
   python benchmark_ping.py < bench/bench_$i.log > bench/bench_$i.res
 done
 #echo \"$(join_by " " ${size[@]})\"
---
+++ lightControler/run_bench.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./startControlers.sh $2 $1 5672 > bench/bench_$2.log
